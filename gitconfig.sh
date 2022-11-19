575843058d736be29c17bb0e323d8feac15babb9
Michael Coyne <mjc@hey.com>
2022-11-19 15:36:32 -0500

    chore: add git config script

---
+++ gitconfig.sh
@@ -0,0 +1,5 @@
+git config --global alias.st 'status'
+git config --global alias.rpull 'pull --rebase --stat'
+git config --global alias.wdiff "diff --word-diff=color"
+git config --global user.email 'mjc@hey.com'
+git config --global user.name 'Michael Coyne'
