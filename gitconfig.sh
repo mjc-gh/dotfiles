a1110915de080e5b82e8c3ab44480339bbf0107b
Michael Coyne <82063+mjc-gh@users.noreply.github.com>
2026-03-23 15:35:43 -0400

    Configure delta for enhanced git diff experience
    Add delta configuration for improved git diff output.

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,3 +3,9 @@ git config --global alias.rpull 'pull --rebase --stat'
 git config --global alias.wdiff "diff --word-diff=color"
 git config --global user.email 'mjc@hey.com'
 git config --global user.name 'Michael Coyne'
+
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global delta.navigate true
+git config --global delta.dark true  # or `delta.light true`, or omit for auto-detection
+git config --global merge.conflictStyle zdiff3
